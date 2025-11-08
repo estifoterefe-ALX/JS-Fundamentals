193ca5adb90f5b95b3c4d429d5959b5c95b77279
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2025-11-08 10:23:16 +0300

    fix

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,5 +1,5 @@
 const { argv } = require("node:process");
 const display = parseInt(argv[2])
-  ? `My number:${parseInt(argv[2])}`
+  ? `My number: ${parseInt(argv[2])}`
   : "Not a number";
 console.log(display);
