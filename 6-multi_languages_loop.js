558b3e8cf76bcace6962937a3a1a485699d678c8
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2025-11-08 10:26:38 +0300

    6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+const firstLine = "C is fun";
+const secondLine = "Python is cool";
+const thirdLine = "JavaScript is amazing";
+const lines = [firstLine, secondLine, thirdLine];
+lines.forEach((val, index) => {
+  console.log(val);
+});
