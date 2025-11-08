770271f0ed429ff70073cacbd11b5f40d17ab509
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2025-11-08 10:44:03 +0300

    fixes

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+const { argv } = require("node:process");
+
+const num = parseInt(argv[2]);
+
+if (!num) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log("X".repeat(num));
+  }
+}
